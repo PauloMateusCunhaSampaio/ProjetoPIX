0679c50f439bc27a9b79dac75d8c5cd295a8e72e
Gabriel Gomes Attie <67513919+attieGabriel@users.noreply.github.com>
2021-01-20 18:56:10 -0300

    Update Poupanca.java

--- poo pix/conta/Poupanca.java
+++ poo pix/conta/Poupanca.java
@@ -10,7 +10,7 @@ public class Poupanca extends conta {
     public void sacar(double valor) {
         int meses=0;//tem q colocar a quantidade de meses q o dinheiro ficou na conta
         if(saldo>valor || meses<12)
-        {System.out.println("Saldo insuficiente ou tempo limie noa atingido");}
+        {System.out.println("Saldo insuficiente ou tempo limite não atingido");}
         else{
             saldo-=valor;
         }
