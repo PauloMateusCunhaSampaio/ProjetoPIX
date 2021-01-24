5652a90a80e3b5e4c18ba4c5bc3acf12b758455d
Gabriel Gomes Attie <67513919+attieGabriel@users.noreply.github.com>
2021-01-24 16:38:11 -0300

    Update Poupanca.java

--- poo pix/conta/Poupanca.java
+++ poo pix/conta/Poupanca.java
@@ -5,25 +5,22 @@ public class Poupanca extends conta {
     public void depositar(double valor) {
         saldo+=valor;
     }
-    
     protected int tempo;
     public int getTempo() {
         return tempo;
     }
+
     protected void setTempo(int tempo) {
         this.tempo = tempo;
     }
-    
     public boolean Limite(int tempo){
         if(tempo>12)
         {return true;}
         else{return false;}
     }
-    
     @Override
     public void sacar(double valor) {
-        int meses=0;
-        if(Limite(meses))
+        if(Limite(tempo))
         {saldo-=valor;}
     }
 }
